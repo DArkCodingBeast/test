4667e2826965bfb3d9732074a6f46392db354abc
Garomine <minecrafstorymode@gmail.com>
2025-03-27 09:50:27 +0100

    nique ta mere

--- Project-Info_Best-Of-The-Best/ObjetMobile.cc
+++ Project-Info_Best-Of-The-Best/ObjetMobile.cc
@@ -3,18 +3,32 @@
 #include <array>
 #include "PointMateriel.h"
 
-class ObjetMateriel {
+class ObjetMobile {
 
     public:
 
+    //contstruct
+    ObjetMobile(std::vector <double> parameters) 
+        : parameters(parameters), derivee_temporelle(parameters.size(), 0.0) {}
+
+    //Methode
     virtual void evolution(double t) = 0;
 
+    std::ostream& affiche() const {
+        std::ostream& sortie;
+        sortie << "nique ta race";
+        return sortie;
+      }
 
 
     protected:
 
-    std::vector <double> parmaterers;
-    std::vector <double, dim()> dérivée_temporelle_des_paramètres;
-    size_t dim() {return parameters.size();}
+    std::vector <double> parameters;
+    std::vector <double> derivee_temporelle;
 
 };
+
+std::ostream& operator<<(std::ostream& sortie, const ObjetMobile& obj){
+    sortie << affiche();
+
+    return sortie;}
