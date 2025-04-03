9cf92ada2651f1e0d26241057861819e6be3db72
DArkCodingBeast <gregory.serillon@gmail.com>
2025-04-03 12:47:02 +0200

    Update PointMateriel.cc

--- Project-Info_Best-Of-The-Best/PointMateriel.cc
+++ Project-Info_Best-Of-The-Best/PointMateriel.cc
@@ -1,33 +1,19 @@
 #include "PointMateriel.h"
+#include "Vecteur.h"
 
-
-GravitationConstante::GravitationConstante(std::array<double, 3> const& grav) : gravitation(grav) {}
-GravitationConstante::std::array<double,3> force(PointMateriel const& p1, double temps) {
-    return {gravitation[0] * p1.Get_masse(),
-            gravitation[1] * p1.Get_masse(),
-            gravitation[2] * p1.Get_masse()};
+GravitationConstante::GravitationConstante(Vecteur const& grav) : gravitation(grav) {}
+Vecteur GravitationConstante::force(PointMateriel const& p1, double temps) {
+    return {gravitation.mult(p1.Get_masse())};
 }
 
 
-PointMateriel::PointMateriel(PointMateriel const& autre): vect_etat(autre.vect_etat), vect_derivee(autre.vect_derivee), masse(autre.masse), ChampForce(autre.ChampForce) {}
-PointMateriel::const std::array<double,3> Get_Position() const {return vect_etat;}	
-PointMateriel::const std::array<double,3> Get_Derivative_Pos() const {return vect_derivee;}		
-PointMateriel::const double Get_masse() const {return masse;}
-PointMateriel::const std::array<double,3> Get_Champ() const {return ChampForce;}
-PointMateriel::void set_vect_etat(std::array<double,3> autre) {vect_etat = autre;}
-PointMateriel::std::array<double,3> evolution(double temps) {return {ChampForce[0]/masse,ChampForce[1]/masse,ChampForce[2]/masse};}
-
-
-std::ostream& operator<<(std::ostream& sortie,std::array<double,3> const& autre)
-{sortie << "[";
-    for (size_t i = 0; i < autre.size(); ++i) {
-        sortie << autre[i];
-        if (i != autre.size() - 1) {
-            sortie << ", ";
-        }
-    }
-    sortie << "]";
-    return sortie;}
+PointMateriel:: PointMateriel(PointMateriel const& autre): vect_etat(autre.vect_etat), vect_derivee(autre.vect_derivee), masse(autre.masse), ChampForce(autre.ChampForce) {}
+const Vecteur PointMateriel:: Get_Position() const {return vect_etat;}	
+const Vecteur PointMateriel:: Get_Derivative_Pos() const {return vect_derivee;}		
+const double PointMateriel:: Get_masse() const {return masse;}
+const Vecteur PointMateriel:: Get_Champ() const {return ChampForce;}
+void PointMateriel:: set_vect_etat(Vecteur autre) {vect_etat = autre;}
+Vecteur PointMateriel:: evolution(double temps) {return {ChampForce.mult(1/masse)};}
 
 
 std::ostream& operator<<(std::ostream& sortie,PointMateriel const& autre)
