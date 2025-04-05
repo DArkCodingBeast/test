544591c6f2fc59347d795b903baf14d8b4ddc078
Garomine <minecrafstorymode@gmail.com>
2025-04-05 15:26:22 +0200

    kikikiik

--- Project-Info_Best-Of-The-Best/Dessinable.cc
+++ Project-Info_Best-Of-The-Best/Dessinable.cc
@@ -10,12 +10,34 @@ public:
     virtual dessine_sur(SupportADessin& support) = 0;
 };
 
-class SupportADessin
-{
+class SupportADessin {
 public:
 virtual ~SupportADessin() = default;
+SupportADessin(SupportADessin const&)            = delete;
+SupportADessin& operator=(SupportADessin const&) = delete;
 
 virtual void dessine(PointMateriel const&) = 0;
 virtual void dessine(Systeme const&) = 0;
 virtual void dessine(Solide const&) = 0; 
 };
+
+class TextViewer : public SupportADessin {
+    public:
+    TextViewer(std::ostream& flot)
+        : flot(flot)
+      {}
+    virtual ~TextViewer() = default;
+    
+    TextViewer(TextViewer const&)            = delete;
+    TextViewer& operator=(TextViewer const&) = delete;
+     
+    virtual void dessine(Contenu const& a_dessiner) override;
+     
+    private:
+    std::ostream& flot;
+    };
+
+/*
+virtual void dessine_sur(SupportADessin& support) override
+{ support.dessine(*this); }
+ */
