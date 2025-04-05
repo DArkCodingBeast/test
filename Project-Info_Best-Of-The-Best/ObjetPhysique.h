4677a32cd6618826b5078c97b2258e4b3e43d991
DArkCodingBeast <gregory.serillon@gmail.com>
2025-04-05 12:31:10 +0200

    Create ObjetPhysique.h

---
+++ Project-Info_Best-Of-The-Best/ObjetPhysique.h
@@ -0,0 +1,99 @@
+#include "ObjetMobile.h"
+#include <string>
+
+
+class Contrainte{
+public:
+
+    virtual Vecteur applique_force(ObjetPhysique const& obj,Vecteur force, double temps) = 0;
+    virtual Vecteur position(ObjetPhysique const& obj) = 0;
+    virtual Vecteur vitesse(ObjetPhysique const& obj) = 0;
+};
+
+
+
+class Libre : public Contrainte{
+    public:
+    
+        Vecteur applique_force(ObjetPhysique const& obj,Vecteur force, double temps)
+        {if (obj.get_masse() == 0) {return {force};}
+        return {force.mult(1 / obj.get_masse())};}
+    
+        Vecteur position(ObjetPhysique const& obj) {return obj.getParam();}
+        Vecteur vitesse(ObjetPhysique const& obj) {return obj.getDerive();}
+    };
+    
+
+class ChampForces{
+
+public :
+    virtual Vecteur force(ObjetPhysique const&, double t) = 0;
+};
+
+
+
+
+class ObjetPhysique : public ObjetMobile{
+
+private :
+    Contrainte& cont;
+    ChampForces& champ;
+    unsigned int dim;
+    double masse;
+    double charge;
+
+
+public:
+
+    ObjetPhysique (ObjetPhysique const& autre): cont(autre.get_cont()), champ(autre.get_champ()), dim(autre.get_dim()), masse(autre.get_masse()), charge(autre.get_charge()){}
+
+    ObjetPhysique (Contrainte& cont, ChampForces& champ, unsigned int dim): cont(cont), champ(champ), dim(dim){}
+
+    Contrainte const& get_cont() {return cont;}
+
+    ChampForces const& get_champ() {return champ;}
+
+    const unsigned int get_dim() {return dim;}
+
+    const double get_masse() {return masse;}
+
+    const double get_charge() {return charge;}
+
+    Vecteur force(double t = 0) const
+    {return champ.force(*this,t);}              // A finir ces trois fonction puis adapter les classes de semaine avant comme sous classes de celle ci
+    
+    Vecteur position() const
+    {return cont.position(*this);}
+
+    Vecteur vitesse() const
+    {return cont.vitesse(*this);}
+};
+
+
+std::ostream& operator<<(std::ostream& sortie, ChampForces const& autre)
+{ return sortie;}
+
+
+std::ostream& operator<<(std::ostream& sortie, Contrainte const& autre)
+{ return sortie;}
+
+
+std::ostream& operator<<(std::ostream& sortie, ObjetPhysique & autre)
+{sortie << autre.get_cont() << autre.get_champ() << autre.get_dim() << autre.get_charge() << autre.get_masse() << std::endl;
+ return sortie;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
