2a6f6c75ab0282b5f1f1ee8c721444656ab7f302
Garomine <minecrafstorymode@gmail.com>
2025-04-05 17:07:27 +0200

    i am ze goat

--- Project-Info_Best-Of-The-Best/Dessinable.cc
+++ Project-Info_Best-Of-The-Best/Dessinable.cc
@@ -2,11 +2,52 @@
 #include <memory>
 
 class SupportADessin;
+class Dessinable;
 
 class Systeme {
 protected:
-std::vector<std::unique_ptr<Dessinable> objets;
+std::vector<std::unique_ptr<Dessinable>> objets;
+std::vector<std::unique_ptr<Contrainte>> contraintes;
+std::vector<std::unique_ptr<ChampForces>> champs;
+IntegrateurEulerCromer integrateur;
+double temps;
+
+public:
+Systeme() : temps(0.0) {}
+
+std::vector<std::unique_ptr<Dessinable>> getObjets() const {return objets;}
+std::vector<std::unique_ptr<Contrainte>> getContraintes() const {return contraintes;}
+std::vector<std::unique_ptr<ChampForces>> getChamp() const {return champs;}
+double getTemps() {return temps;}
+
+void ajouter_objet(std::unique_ptr<Dessinable> objet) {
+    objets.push_back(objet);
+}
+
+void ajouter_contrainte(std::unique_ptr<Contrainte> contrainte) {
+    contraintes.push_back(contrainte);
+}
+
+void ajouter_champ(std::unique_ptr<ChampForces> champ) {
+    champs.push_back(champ);
+}
+
+void dessine_sur(SupportADessin& support) const {
+    for (const auto& objet : objets) {
+        objet->dessine_sur(support);
+    }
+}
 };
+
+
+std::ostream& operator<<(std::ostream& sortie, Systeme const& systeme) {
+    sortie << "Systeme : à t = " << systeme.temps << std::endl;
+    for (size_t i = 0; i < systeme.getObjets.size(); ++i){
+        sortie << "Objet no " << i + 1 << " :" << std::endl;
+        sortie << systeme.getObjets()[i] << std::endl << std::endl;
+    }
+    return sortie;
+}
 
 class Dessinable {
 public:
