1d7c9190a330810ce84203e4d25bd4fb02195923
Garomine <minecrafstorymode@gmail.com>
2025-04-05 12:29:36 +0200

    e

---
+++ Project-Info_Best-Of-The-Best/Dessin.cc
@@ -0,0 +1,16 @@
+#include "PointMateriel.h"
+
+class Systeme {};
+
+class Dessin {};
+
+class SupportADessin
+{
+public:
+virtual ~SupportADessin() = default;
+// on suppose ici que les supports ne seront ni copiés ni déplacés
+virtual void dessine(PointMateriel const&) = 0;
+virtual void dessine(Systeme const&) = 0;
+virtual void dessine(Solide const&) = 0; // exemple, non abordé dans ce projet
+// ... autres choses que vous voudriez « dessiner »...
+};
