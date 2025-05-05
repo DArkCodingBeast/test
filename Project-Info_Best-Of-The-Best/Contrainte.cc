a96eafde8586082f3e907b38b63513624a5438a2
Garomine <minecrafstorymode@gmail.com>
2025-05-05 20:13:51 +0200

    Cleaning Visuals

--- Project-Info_Best-Of-The-Best/Contrainte.cc
+++ Project-Info_Best-Of-The-Best/Contrainte.cc
@@ -8,10 +8,16 @@ Vecteur Libre:: applique_force(ObjetPhysique const& obj,Vecteur force, double te
     return force.mult(1 / obj.get_masse());
 }
 
-Vecteur Libre::position(ObjetPhysique const& obj)     {return obj.getParam();}
-Vecteur Libre::vitesse(ObjetPhysique const& obj)  {return obj.getDerive();}
-void Libre::affiche(std::ostream& sortie) const {sortie << "Aucune contrainte n'est appliqué sur l'objet." << std::endl;}
-
-std::ostream& operator<<(std::ostream& sortie, Contrainte const& autre) {
+Vecteur Libre::position(ObjetPhysique const& obj){
+    return obj.getParam();
+}
+Vecteur Libre::vitesse(ObjetPhysique const& obj){
+    return obj.getDerive();
+}
+void Libre::affiche(std::ostream& sortie) const{
+    sortie << "Aucune contrainte n'est appliqué sur l'objet." << std::endl;
+}
+std::ostream& operator<<(std::ostream& sortie, Contrainte const& autre){
     autre.affiche(sortie);
-    return sortie;}
+    return sortie;
+}
--- Project-Info_Best-Of-The-Best/constantes.h
+++ Project-Info_Best-Of-The-Best/constantes.h
@@ -2,5 +2,5 @@
 #include "Vecteur.h"
 namespace constantes {
 static constexpr double G(6.6743015*1e-11);
-static const Vecteur g(0.0, 0.0, -9.81); // ou autre initialisation similaire avec vos constru
+static const Vecteur g(0.0, 0.0, -9.81); 
 }
