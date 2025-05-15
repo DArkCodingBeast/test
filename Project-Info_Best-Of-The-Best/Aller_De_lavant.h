d2d3ba260f768b615d692769aec6a8477524a6df
DArkCodingBeast <gregory.serillon@gmail.com>
2025-05-15 12:14:52 +0200

    Update Aller_De_lavant.h

--- Project-Info_Best-Of-The-Best/Aller_De_lavant.h
+++ Project-Info_Best-Of-The-Best/Aller_De_lavant.h
@@ -5,12 +5,14 @@ class ObjetPhysique;
 
 class Aller_De_lavant : public Contrainte
 {private: 
-double vit;
-    double temps
+	double vit;
+    double temps;
 
 public:
     Aller_De_lavant(double, double = 0 );
     Vecteur position(ObjetPhysique const& ) override;
     Vecteur vitesse(ObjetPhysique const& ) override;
     void affiche(std::ostream&) const;
+    void set(double);
 };
+
