89137d1d41ee72d57f7b27dbe2ec22e69fe470a2
Garomine <minecrafstorymode@gmail.com>
2025-05-15 10:47:42 +0200

    pmat sph

--- Project-Info_Best-Of-The-Best/PointMateriel.h
+++ Project-Info_Best-Of-The-Best/PointMateriel.h
@@ -15,3 +15,14 @@ class PointMateriel : public ObjetPhysique {
     };
 
 std::ostream& operator<<(std::ostream&, const PointMateriel&);
+
+class PointMat_Sphere : public PointMateriel{
+
+public:
+    PointMat_Sphere(PointMateriel const&);
+    PointMat_Sphere(Vecteur, Vecteur, std::shared_ptr<ChampForces> /*champ*/, std::shared_ptr<Contrainte> /*cont*/, double, double = 0, unsigned int = 3);
+    PointMat_Sphere(Vecteur, std::shared_ptr<ChampForces> /*champ*/, std::shared_ptr<Contrainte> /*cont*/, double, double = 0, unsigned int = 3);
+    PointMat_Sphere(Vecteur, Vecteur, double, double = 0, unsigned int = 3);
+
+
+};
