f1efe2ce6462fdba189727da0fef9856b1f7e65c
Garomine <minecrafstorymode@gmail.com>
2025-05-15 12:52:15 +0200

    Update testSysteme.cc

--- Project-Info_Best-Of-The-Best/testSysteme.cc
+++ Project-Info_Best-Of-The-Best/testSysteme.cc
@@ -1,4 +1,4 @@
-#include "Systeme.h"
+/*#include "Systeme.h"
 #include "PointMateriel.h"
 #include "ForceCentral.h"
 #include "Contrainte.h"
@@ -41,18 +41,57 @@ int main() {
     systeme.ajouter_champ(champGravitation);
     cout << systeme << endl;
     //tesitng switching stuff
-    auto contrainte_s = make_shared<Cont_spherique>(3.0);
+    auto contrainte_s = make_shared<Cont_spherique>(1.0);
     systeme.ajouter_contrainte(contrainte_s);
     systeme.donner_contrainte(0,1);
     cout << systeme << endl;
     // Simulate the system
-    double dt = 0.5;
-    for (int i = 0; i < 5; ++i) {
+    double dt = 1e-3;
+    for (int i = 0; i < 1000; ++i) {
         systeme.evolue(dt);
         systeme.dessine_sur(viewer);
-        cout << "temps = " << (i+1)*0.5 << endl << *systeme.getObjets()[0] << endl;
+        if (i%100 == 0){
+        cout << "temps = " << systeme.getTemps() << endl << *systeme.getObjets()[0] << endl;}
+    }
+    cout << "Simulation complete." << endl;
+
+    return 0;
+}*/
+
+#include "Systeme.h"
+#include "PointMateriel.h"
+#include "ForceCentral.h"
+#include "Contrainte.h"
+#include "SupportTextDessin.h"
+#include "Cont_spherique.h"
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main() {
+
+    Systeme systeme;
+    
+    Vecteur gravitation(0.0, 0.0, -9.81);
+    auto champGravitation = make_shared<GravitationConstante>(gravitation);
+
+    auto contrainte_s = make_shared<Cont_spherique>(2.2);
+    Vecteur position1({M_PI / 6, 0});
+    Vecteur vitesse1({0.0, 1});
+    auto objet1 = make_unique<PointMateriel>(position1, vitesse1, champGravitation, contrainte_s, 0.1);
+
+    systeme.ajouter_objet(move(objet1));
+
+    cout << systeme << endl;
+
+    double dt = 0.01;
+    for (int i = 0; i < 10; ++i) {
+        systeme.evolue(dt);
+        cout << "temps = " << systeme.getTemps() << endl << *systeme.getObjets()[0] << endl;
     }
     cout << "Simulation complete." << endl;
 
     return 0;
 }
+
