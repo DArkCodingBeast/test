15464640ff7bd7b1023f4f56d96d126d1a7133f3
Garomine <minecrafstorymode@gmail.com>
2025-05-15 12:15:09 +0200

    Update PointMateriel.h

--- Project-Info_Best-Of-The-Best/PointMateriel.h
+++ Project-Info_Best-Of-The-Best/PointMateriel.h
@@ -1,28 +1,25 @@
-#include "SupportTextDessin.h"
-#pragma once
+#include "PointMateriel.h"
 
-class PointMateriel : public ObjetPhysique {	
-    public:
-        PointMateriel(PointMateriel const&);
-        PointMateriel (Vecteur, Vecteur, std::shared_ptr<ChampForces> /*champ*/, std::shared_ptr<Contrainte> /*cont*/, double, double = 0, unsigned int = 3);
-        PointMateriel(Vecteur, std::shared_ptr<ChampForces> /*champ*/, std::shared_ptr<Contrainte> /*cont*/, double, double = 0, unsigned int = 3);
-        PointMateriel(Vecteur, Vecteur, double, double = 0, unsigned int = 3);
-        
-        virtual void dessine_sur(SupportADessin&) override;
-        virtual Vecteur evolution(double) override;
-        void affiche(std::ostream&) const;
-        
-    };
-
-std::ostream& operator<<(std::ostream&, const PointMateriel&);
-
-class PointMat_Sphere : public PointMateriel{
-
-public:
-    PointMat_Sphere(PointMateriel const&);
-    PointMat_Sphere(Vecteur, Vecteur, std::shared_ptr<ChampForces> /*champ*/, std::shared_ptr<Contrainte> /*cont*/, double, double = 0, unsigned int = 3);
-    PointMat_Sphere(Vecteur, std::shared_ptr<ChampForces> /*champ*/, std::shared_ptr<Contrainte> /*cont*/, double, double = 0, unsigned int = 3);
-    PointMat_Sphere(Vecteur, Vecteur, double, double = 0, unsigned int = 3);
+PointMateriel::PointMateriel(Vecteur parameters, Vecteur derive, std::shared_ptr<ChampForces> champ, std::shared_ptr<Contrainte> cont, double masse, double charge, unsigned int dim): 
+	ObjetPhysique(parameters, derive, cont, champ, masse, charge, dim) {}
+PointMateriel::PointMateriel(Vecteur parameters, std::shared_ptr<ChampForces> champ, std::shared_ptr<Contrainte> cont, double masse, double charge , unsigned int dim): 
+	ObjetPhysique(parameters, cont , champ, masse, charge, dim) {}
+PointMateriel::PointMateriel(Vecteur parameters, Vecteur derive, double masse, double charge, unsigned int dim): 
+	ObjetPhysique(parameters, derive, masse, charge, dim) {}
+	
+PointMateriel::PointMateriel(PointMateriel const& autre): ObjetPhysique (autre){}	
 
 
-};
+void PointMateriel::dessine_sur(SupportADessin&) {}
+Vecteur PointMateriel::evolution(double temps){
+	return cont->applique_force(*this, champ->force(*this,temps), temps);
+}
+void PointMateriel::affiche(std::ostream& sortie) const {
+	ObjetPhysique::affiche(sortie);
+}
+std::ostream& operator<<(std::ostream& sortie, const PointMateriel& p){
+	p.affiche(sortie);
+	return sortie;	
+}
+
+
