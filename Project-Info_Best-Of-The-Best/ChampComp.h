7555aafc63cc3bd746289e72d51f5155a1d76ea8
DArkCodingBeast <gregory.serillon@gmail.com>
2025-05-15 12:15:29 +0200

    Update ChampComp.h

--- Project-Info_Best-Of-The-Best/ChampComp.h
+++ Project-Info_Best-Of-The-Best/ChampComp.h
@@ -1,1 +1,20 @@
-#include "ChampForce"
+#include <vector>
+#include "ChampForces.h"
+#pragma once
+
+class ObjetPhysique;
+
+class ChampComp : public ChampForces{
+private :
+	std::vector<ChampForces> compo_champ;
+	
+	
+public:
+	ChampComp(ChampComp const &);
+	ChampComp(ChampForces const &);
+	ChampComp();
+	Vecteur force(ObjetPhysique const &, double);
+
+
+
+	};
