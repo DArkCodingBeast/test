8f7080612247ecbd42342ee4aa3881f6c19bea95
DArkCodingBeast <gregory.serillon@gmail.com>
2025-05-15 10:16:44 +0200

    Update Aller_De_lavant.h

--- Project-Info_Best-Of-The-Best/Aller_De_lavant.h
+++ Project-Info_Best-Of-The-Best/Aller_De_lavant.h
@@ -1,1 +1,13 @@
-#include "Contrainte.h"
+#include "ObjetPhysique.h"
+#pragma once
+
+class ObjetPhysique;
+
+class Aller_De_lavant : public Contrainte
+{
+public:
+	Cont_spherique(double );
+    Vecteur position(ObjetPhysique const& ) override;
+    Vecteur vitesse(ObjetPhysique const& ) override;
+    void affiche(std::ostream&) const;
+};
