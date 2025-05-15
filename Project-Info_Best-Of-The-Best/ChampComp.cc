fc4ece0ed1ab2124dc99805e214c33f994874487
DArkCodingBeast <gregory.serillon@gmail.com>
2025-05-15 12:15:15 +0200

    Update ChampComp.cc

--- Project-Info_Best-Of-The-Best/ChampComp.cc
+++ Project-Info_Best-Of-The-Best/ChampComp.cc
@@ -1,1 +1,11 @@
+#include "ChampForces.h"
 
+
+
+
+
+
+ChampComp::ChampComp(ChampComp const & tab) : compo_champ(tab) {}
+ChampComp::ChampComp(ChampForces const & champ1) : compo_champ.push_back(champ1) {}
+ChampComp::ChampComp() : compo_champ({}) {}
+ChampComp::Vecteur force(ObjetPhysique const & obj, double t);
