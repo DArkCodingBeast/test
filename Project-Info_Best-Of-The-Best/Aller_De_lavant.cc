7f432aafd227ad9dca1bf43f674c92f878644847
DArkCodingBeast <gregory.serillon@gmail.com>
2025-05-15 12:14:33 +0200

    Update Aller_De_lavant.cc

--- Project-Info_Best-Of-The-Best/Aller_De_lavant.cc
+++ Project-Info_Best-Of-The-Best/Aller_De_lavant.cc
@@ -5,6 +5,8 @@ class ObjetPhysique;
 
 Aller_De_lavant::Aller_De_lavant(double vitesse, double temps) : vit(vitesse), temps(temps) {}
     
-Vecteur Aller_De_lavant::position() {return }
- 
+Vecteur Aller_De_lavant::position(ObjetPhysique const& obj) {return Vecteur(vit*temps , obj.getParam().get_vect()[1] , obj.getParam().get_vect()[2]);}
+Vecteur Aller_De_lavant::vitesse(ObjetPhysique const& obj) {return Vecteur(vit , obj.getDerive().get_vect()[1] , obj.getDerive().get_vect()[2]);}
+void Aller_De_lavant::affiche(std::ostream& sortie) const {sortie << " l'objet est deplacé sur l'axe des x, avec une vitesse de " << vit << "m/s pendant " << temps << "seconde "<< std::endl;}
+void Aller_De_lavant::set(double t) {temps = t;}
 
